8272d5dac096c8e9899fda8c7abb102ad1afa28e
mikeSxz <mi.angel.itics@gmail.com>
2022-07-12 08:01:45 -0500

    Replacing spring java configuration with spring xml configuration

--- src/main/java/com/handyitservices/springboot/DependencyInjectionApplication.java
+++ src/main/java/com/handyitservices/springboot/DependencyInjectionApplication.java
@@ -4,10 +4,11 @@ import com.handyitservices.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("com.handyitservices")
+@ImportResource("classpath:/spring/spring-config.xml")
+//@ComponentScan("com.handyitservices") we commented this because we will start using xml configuration instead of java configuration
 public class DependencyInjectionApplication {
 
     public static void main(String[] args) {
