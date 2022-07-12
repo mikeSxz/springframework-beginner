d89dda3db951f91134a273f71eb328c3da88bea5
mikeSxz <mi.angel.itics@gmail.com>
2022-07-12 10:01:20 -0500

    removing all annotations in controller and replace it with xml based configurations with <property> tag

--- src/main/java/com/handyitservices/controllers/GreetingController.java
+++ src/main/java/com/handyitservices/controllers/GreetingController.java
@@ -1,8 +1,6 @@
 package com.handyitservices.controllers;
 
 import com.handyitservices.services.HelloWorldService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 //@Controller
 public class GreetingController {
@@ -10,19 +8,17 @@ public class GreetingController {
     private HelloWorldService helloWorldServiceGerman;
     private HelloWorldService helloWorldServiceFrench;
 
-    @Autowired //injected by type
+
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
-    @Autowired // by method name
-    @Qualifier("helloWorldServiceGerman")
+
     public void setHelloWorldServiceGerman(HelloWorldService helloWorldServiceGerman) {
         this.helloWorldServiceGerman = helloWorldServiceGerman;
     }
 
-    @Autowired //by named bean
-    @Qualifier("french")
+
     public void setHelloWorldServiceFrench(HelloWorldService helloWorldServiceFrench) {
         this.helloWorldServiceFrench = helloWorldServiceFrench;
     }
